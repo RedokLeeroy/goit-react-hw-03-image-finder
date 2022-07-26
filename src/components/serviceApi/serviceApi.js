f639e4177d78a5c97ede8aba1f8e19945d122054
RedokLeeroy <Lys1999@ukr.net>
2022-07-27 01:39:00 +0300

    .

--- src/components/serviceApi/serviceApi.js
+++ src/components/serviceApi/serviceApi.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 axios.defaults.baseURL = `https://pixabay.com/api/`;
 const API_KEY = '28342095-bdb3373d4270e11a929e663ef';
 export const Service = (page, queue) => {
-  console.log(page, queue);
   return axios.get('', {
     params: {
       key: API_KEY,
